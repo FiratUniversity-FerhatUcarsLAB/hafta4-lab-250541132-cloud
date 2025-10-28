99d6897288ad409571bfbeb0265fb50553bfde67
250541132-cloud <250541132@firat.edu.tr>
2025-10-28 22:11:25 +0300

    Update GeometrikHesap.java

--- GeometrikHesap.java
+++ GeometrikHesap.java
@@ -1,1 +1,42 @@
+import java.util.Scanner;
+import java.lang.Math;
+import java.text.DecimalFormat;
 
+public class GeometrikHesap {
+
+    private static final double PI = 3.14159;
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        System.out.println("--- Geometrik Hesaplama Programı ---");
+        System.out.print("Lütfen daire yarıçapını (r) giriniz: ");
+
+        double yaricap = scanner.nextDouble();
+
+        double yaricapKare = Math.pow(yaricap, 2);
+        double yaricapKup = Math.pow(yaricap, 3);
+        double daireAlani = PI * yaricapKare;
+        double daireCevresi = 2 * PI * yaricap;
+        double daireCapi = 2 * yaricap;
+        double kureHacmi = (4.0 / 3.0) * PI * yaricapKup;
+        double kureYuzeyAlani = 4 * PI * yaricapKare;
+        String formatBaslik = "%-20s";
+        String formatDeger = "%10s";
+
+        System.out.println("\n--- Hesaplama Sonuçları (Yarıçap = " + df.format(yaricap) + ") ---");
+        System.out.println("------------------------------------");
+
+        System.out.printf(formatBaslik + formatDeger + "\n", "Daire Alanı:", df.format(daireAlani));
+        System.out.printf(formatBaslik + formatDeger + "\n", "Daire Çevresi:", df.format(daireCevresi));
+        System.out.printf(formatBaslik + formatDeger + "\n", "Daire Çapı:", df.format(daireCapi));
+        System.out.printf(formatBaslik + formatDeger + "\n", "Küre Hacmi:", df.format(kureHacmi));
+        System.out.printf(formatBaslik + formatDeger + "\n", "Küre Yüzey Alanı:", df.format(kureYuzeyAlani));
+
+        System.out.println("------------------------------------");
+
+        scanner.close();
+    }
+}
